e214bc77eee297dd3ea1777bcc58313ca54ad7ba
Tunglies <coffee@soe.icu>
2025-01-20 22:43:05 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i32) -> i32{
+    let price = 2;
+    if count > 40 {
+        return count;
+    } else {
+        return count * price;
+    }
+}
 
 // Don't modify this function!
 #[test]
